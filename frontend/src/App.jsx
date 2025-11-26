6f6ff999667857eea41b77fa16912177e18c8857
Pedro Quadrat <pedroquadrat@gmail.com>
2025-11-26 18:50:03 -0300

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [arquivoImagem, setArquivoImagem] = useState(null)
   const [uploading, setUploading] = useState(false)
 
-  const API_BASE = 'https://deploy-automatico-Pq-d3hxfvdbeea5b0b9.canadacentral-01.azurewebsites.net/api';
+  const API_BASE = 'https://deploy-automatico-pq-d3hxfvdbeea5b0b9.canadacentral-01.azurewebsites.net/api';
 
   const carregarObras = () => {
     fetch(`${API_BASE}/getObras`)
